2d0f8ede7edbe5ee9d57b8c1033ecf33444801bc
ren1023 <renyou.1023@gmail.com>
2024-01-22 15:55:37 +0800

    完成會員登入及驗證碼檢查功能

--- front/login.php
+++ front/login.php
@@ -1,5 +1,5 @@
 <h2>第一次購買</h2>
-<img src="./icon/0413.jpg" onclick="location.href='?do=reg'" >
+<img src="./icon/0413.jpg" onclick="location.href='?do=reg'">
 
 
 
@@ -18,14 +18,54 @@
         <td class="tt">驗證碼</td>
         <td class="pp">
             <?php
-            $a=rand(10,99);
-            $b=rand(10,99);
-            $_SESSION['ans']=$a+$b;
-            echo $a."+".$b."=";
+            $a = rand(10, 99);
+            $b = rand(10, 99);
+            $_SESSION['ans'] = $a + $b;
+            echo $a . "+" . $b . "=";
             ?>
-        <input type="text" name="chk" id="chk"></td>
+            <input type="text" name="ans" id="ans">
+        </td>
     </tr>
 </table>
 
 
-<div class="tc"><button>確認</button></div>
+<div class="ct">
+    <button onclick="login('mem')">確認</button>
+</div>
+<script>
+    function login(table) {
+        $.get('./api/chk_ans.php', {ans: $("#ans").val()}, (chk) => {if (parseInt(chk) == 0) {
+                alert("驗證碼錯誤，請重新輸入")
+            } else {
+                $.post('./api/chk_pw.php', {
+                    table,
+                    acc: $('#acc').val(),
+                    pw: $("#pw").val()}, (res) => {
+                    if (parseInt(res) == 0) {
+                        alert("驗證碼錯誤，請重新輸入")
+
+                    } else {
+                        location.href = 'index.php'
+                    }
+                })
+            }
+        })
+    }
+
+
+//     function login(table){
+//     $.get('./api/chk_ans.php',{ans:$("#ans").val()},(chk)=>{
+//         if(parseInt(chk)==0){
+//             alert("驗證碼錯誤，請重新輸入")
+//         }else{
+//             $.post("./api/chk_pw.php",{table,acc:$("#acc").val(),pw:$("#pw").val()},(res)=>{
+//                 if(parseInt(res)==0){
+//                     alert("帳號或密碼錯誤，請重新輸入")
+//                 }else{
+//                     location.href='index.php';
+//                 }
+//             })
+//         }
+//     })
+// }
+</script>
