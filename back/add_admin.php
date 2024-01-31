9a4b9f2b093cd6384cbaf204fe5d0c86948c8e55
ren1023 <renyou.1023@gmail.com>
2024-01-31 10:25:50 +0800

    修改value的拼字錯誤問題

--- back/add_admin.php
+++ back/add_admin.php
@@ -13,23 +13,23 @@
             <td class="tt ct">權限</td>
             <td class="pp">
                 <div>
-                    <input type="checkbox" name="pr[]" vlaue="1">
+                    <input type="checkbox" name="pr[]" value="1">
                     商品分類與管理
                 </div>
                 <div>
-                    <input type="checkbox" name="pr[]" vlaue="2">
+                    <input type="checkbox" name="pr[]" value="2">
                     訂單管理
                 </div>
                 <div>
-                    <input type="checkbox" name="pr[]" vlaue="3">
+                    <input type="checkbox" name="pr[]" value="3">
                     會員管理
                 </div>
                 <div>
-                    <input type="checkbox" name="pr[]" vlaue="4">
+                    <input type="checkbox" name="pr[]" value="4">
                     頁尾版權區管理
                 </div>
                 <div>
-                    <input type="checkbox" name="pr[]" vlaue="5">
+                    <input type="checkbox" name="pr[]" value="5">
                     最新消息管理
                 </div>
             </td>
