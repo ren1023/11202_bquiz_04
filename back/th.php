8e1af0628687f37be960a853b28913a7d7f59c19
ren1023 <renyou.1023@gmail.com>
2024-01-31 15:28:56 +0800

    建立新增商品表單頁面

--- back/th.php
+++ back/th.php
@@ -93,7 +93,7 @@
 
 <!-- 商品管理 -->
 <h2 class="ct">商品管理</h2>
-<div class="ct"><button>新增商品</button></div>
+<div class="ct"><button onclick="location.href='?do=add_goods'">新增商品</button></div>
 <table class="all">
     <tr class="tt ct">
         <th>編號</th>
