3eff01d0cf942fa3bd5d2f8481007a19b5fca10c
ren1023 <renyou.1023@gmail.com>
2024-02-16 15:35:54 +0800

    完成點選我要購買,在購物車中顯示商品

--- front/buycart.php
+++ front/buycart.php
@@ -1,24 +1,53 @@
 <?php
-// echo "test";
-if(isset($_GET['id'])){
-    $_SESSION['cart'][$_GET['id']]=$_GET['qt'];
+if (isset($_GET['id'])) { //還未登入，就可以加購物車
+    $_SESSION['cart'][$_GET['id']] = $_GET['qt'];
 }
-if(!isset($_SESSION['mem'])){//如果session不存在時，就回到login這個頁！
+
+if (!isset($_SESSION['mem'])) { //如果session不存在時，就回到login這個頁！
     to("?do=login");
 }
 
 echo "<h2 class='ct'>{$_SESSION['mem']} 的購物車</h2>";
 
 
-if(!isset($_SESSION['cart'])){
+if (!isset($_SESSION['cart'])) { //如果沒有購物車，則
 
     echo "<h2 class='ct'>購物車中尚無商品<h2>";
-}else{
-
-    dd($_SESSION['cart']);
 }
+// else{
 
-
-
-
+//     dd($_SESSION['cart']);
+// }
 ?>
+<table class="all">
+    <tr class="tt ct">
+        <td>編號</td>
+        <td>商品名稱</td>
+        <td>數量</td>
+        <td>庫存量</td>
+        <td>單價</td>
+        <td>小計</td>
+        <td>刪除</td>
+    </tr>
+    <?php
+    foreach ($_SESSION['cart'] as $id => $qt) {
+        $goods = $Goods->find($id);
+    ?>
+
+        <tr class="pp ct">
+            <td><?= $goods['no']; ?></td>
+            <td><?= $goods['name']; ?></td>
+            <td><?= $qt; ?></td>
+            <td><?= $goods['stock']; ?></td>
+            <td><?= $goods['price']; ?></td>
+            <td><?= $goods['price'] * $qt; ?></td>
+            <td><img src="./icon/0415.jpg"></td>
+        </tr>
+    <?php
+    }
+    ?>
+</table>
+<div class="ct">
+    <img src="./icon/0411.jpg" >
+    <img src="./icon/0412.jpg" >
+</div>
--- front/detail.php
+++ front/detail.php
@@ -58,6 +58,13 @@ $goods=$Goods->find($_GET['id']);
 
 <div class="tt ct">
     購買數量：
-    <input type="number" name="" value="1" style="width:50px">
-    <img src="./icon/0402.jpg" alt="">
+    <input type="number" id="qt" value="1" style="width:50px">
+    <img src="./icon/0402.jpg" onclick="buy()">
 </div>
+<script>
+    function buy(){
+        let id=<?=$_GET['id'];?>;
+        let qt=$("#qt").val()
+        location.href=`?do=buycart&id=${id}&qt=${qt}`
+    }
+</script>
