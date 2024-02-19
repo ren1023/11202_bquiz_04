64cfc281bdf55baa32e1272ff5fcaedd71c7b3f8
ren1023 <renyou.1023@gmail.com>
2024-02-19 11:04:26 +0800

    update:當登出時，就刪除購車的session

--- api/logout.php
+++ api/logout.php
@@ -3,4 +3,8 @@ session_start();
 
 unset($_SESSION['mem'],$_SESSION['admin']);
 
+if(isset($_SESSION['cart'])){//當登出時，就刪除購車的session(此功能要符合當時需求)
+    unset($_SESSION['cart']);
+}
+
 header('location:../index.php');
